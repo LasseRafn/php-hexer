7a7b391d0e09ccb978bcacacaa4d59d80615475b
Lasse Rafn <lasserafn@gmail.com>
2017-06-21 05:31:51 +0000

    Apply fixes from StyleCI

--- tests/HexToRgbTest.php
+++ tests/HexToRgbTest.php
@@ -4,50 +4,51 @@ use PHPUnit\Framework\TestCase;
 
 class HexToRgbTest extends TestCase
 {
-    public function test_can_convert_to_rgb() {
-    	// Black
-	    $hex = new \LasseRafn\Hexer\Hex('#000');
+    public function test_can_convert_to_rgb()
+    {
+        // Black
+        $hex = new \LasseRafn\Hexer\Hex('#000');
 
-	    $this->assertEquals([
-	    	'r' => 0,
-	        'g' => 0,
-	        'b' => 0
-	    ], $hex->toRgb());
+        $this->assertEquals([
+            'r' => 0,
+            'g' => 0,
+            'b' => 0,
+        ], $hex->toRgb());
 
-	    // White
-	    $hex = new \LasseRafn\Hexer\Hex('#fff');
+        // White
+        $hex = new \LasseRafn\Hexer\Hex('#fff');
 
-	    $this->assertEquals([
-	    	'r' => 255,
-	        'g' => 255,
-	        'b' => 255
-	    ], $hex->toRgb());
+        $this->assertEquals([
+            'r' => 255,
+            'g' => 255,
+            'b' => 255,
+        ], $hex->toRgb());
 
-	    // 6 chars
-	    $hex = new \LasseRafn\Hexer\Hex('#ff000');
+        // 6 chars
+        $hex = new \LasseRafn\Hexer\Hex('#ff000');
 
-	    $this->assertEquals([
-	    	'r' => 255,
-	        'g' => 0,
-	        'b' => 0
-	    ], $hex->toRgb());
+        $this->assertEquals([
+            'r' => 255,
+            'g' => 0,
+            'b' => 0,
+        ], $hex->toRgb());
 
-	    // No hashtag
-	    $hex = new \LasseRafn\Hexer\Hex('007F00');
+        // No hashtag
+        $hex = new \LasseRafn\Hexer\Hex('007F00');
 
-	    $this->assertEquals([
-	    	'r' => 0,
-	        'g' => 127,
-	        'b' => 0
-	    ], $hex->toRgb());
+        $this->assertEquals([
+            'r' => 0,
+            'g' => 127,
+            'b' => 0,
+        ], $hex->toRgb());
 
-	    // With brightness modified
-	    $hex = new \LasseRafn\Hexer\Hex('007F00');
+        // With brightness modified
+        $hex = new \LasseRafn\Hexer\Hex('007F00');
 
-	    $this->assertEquals([
-	    	'r' => 128,
-	        'g' => 255,
-	        'b' => 128
-	    ], $hex->lighten(50)->toRgb());
+        $this->assertEquals([
+            'r' => 128,
+            'g' => 255,
+            'b' => 128,
+        ], $hex->lighten(50)->toRgb());
     }
 }
